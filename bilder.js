2532143570df2ebcf158065ccdd842f3f5dcc880
Günter Laudahn <glaudahn@MacBook-Pro-16.fritz.box>
2025-12-12 20:07:11 +0100

    Initial commit mit Galerie-Dateien

---
+++ bilder.js
@@ -0,0 +1,23 @@
+// bilder.js
+const galerien = {
+    "fantasie": [
+        {src:"img/fantasie/bild1.jpg", caption:"Titel oder Beschreibung 1"},
+        {src:"img/fantasie/bild2.jpg", caption:"Titel oder Beschreibung 2"},
+        {src:"img/fantasie/bild3.jpg", caption:"Titel oder Beschreibung 3"}
+    ],
+    "natur": [
+        {src:"img/natur/bild1.jpg", caption:"Titel oder Beschreibung 1"},
+        {src:"img/natur/bild2.jpg", caption:"Titel oder Beschreibung 2"},
+        {src:"img/natur/bild3.jpg", caption:"Titel oder Beschreibung 3"}
+    ],
+    "stift": [
+        {src:"img/stift/bild1.jpg", caption:"Titel oder Beschreibung 1"},
+        {src:"img/stift/bild2.jpg", caption:"Titel oder Beschreibung 2"},
+        {src:"img/stift/bild3.jpg", caption:"Titel oder Beschreibung 3"}
+    ],
+    "pinsel": [
+        {src:"img/pinsel/bild1.jpg", caption:"Titel oder Beschreibung 1"},
+        {src:"img/pinsel/bild2.jpg", caption:"Titel oder Beschreibung 2"},
+        {src:"img/pinsel/bild3.jpg", caption:"Titel oder Beschreibung 3"}
+    ]
+};
